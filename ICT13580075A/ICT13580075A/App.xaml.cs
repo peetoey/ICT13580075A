090ed1f2a2b44525b07a05f3856816d9a590a832
peetoey <jantaraplin_s@silpakorn.edu>
2017-11-01 02:44:31 +0700

    Finish -> SUICT#13 Visual Studioi #5 Page (2/2)
    Finally !!!

--- ICT13580075A/ICT13580075A/App.xaml.cs
+++ ICT13580075A/ICT13580075A/App.xaml.cs
@@ -13,7 +13,24 @@ namespace ICT13580075A
         {
             InitializeComponent();
 
-            MainPage = new ICT13580075A.AbsolutePage();
+            //var np = new NavigationPage(new NavPage1());
+            var tp = new TabbedPage();
+            tp.Children.Add(new TabPage1());
+            tp.Children.Add(new TabPage2());
+            tp.Children.Add(new TabPage3());
+
+            var cp = new CarouselPage();
+            cp.Children.Add(new TabPage1());
+            cp.Children.Add(new TabPage2());
+            cp.Children.Add(new TabPage3());
+
+            var mp = new MasterDetailPage();
+            mp.Master = new MenuPage();
+            mp.Detail = new NavigationPage(new ICT13580075A.MainPage());
+
+            MainPage = mp;
+
+            //MainPage = new ICT13580075A.NavPage1();
         }
 
         protected override void OnStart()
